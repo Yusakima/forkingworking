562426509bf9bd0e8e220a359a750fc3fbdff3e0
ksenia <yusakimakuro@gmail.com>
2026-03-06 16:46:55 +0300

    Added a window and a button

--- Group4337/MainWindow.xaml.cs
+++ Group4337/MainWindow.xaml.cs
@@ -6,5 +6,11 @@ namespace Group4337
     {
         public MainWindow()
             => InitializeComponent();
+
+        private void Info_4337_Belyaeva_Click(object sender, RoutedEventArgs e)
+        {
+            var infoWindow = new Window1();
+            infoWindow.Show();
+        }
     }
 }
